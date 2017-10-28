ea502c8ab1c48a0639efef53b945d8f28a010ba2
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-10-28 15:18:59 -0400

    Basic GraphQL Endpoint :rocket:

---
+++ StarWars.Api/Models/DroidType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using StarWars.Core.Models;
+
+namespace StarWars.Api.Models
+{
+    public class DroidType : ObjectGraphType<Droid>
+    {
+        public DroidType()
+        {
+            Field(t => t.Id).Description("");
+            Field(t => t.Name, nullable: true).Description("");
+        }
+    }
+}
---
+++ StarWars.Api/Models/StarWarsQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using StarWars.Core.Models;
+
+namespace StarWars.Api.Models
+{
+    public class StarWarsQuery : ObjectGraphType
+    {
+        public StarWarsQuery()
+        {
+            Field<DroidType>("hero", resolve: context => new Droid { Id = 1, Name = "R2-D2" });
+        }
+    }
+}
